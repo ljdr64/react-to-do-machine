c85dac9a9c8d635bb4fe409decbb1f28ba385cb6
Laureano Rodriguez <laure.jd0101@gmail.com>
2024-04-08 23:19:33 -0300

    Buscando TODOs

--- src/App.js
+++ src/App.js
@@ -20,6 +20,12 @@ function App() {
   const completedTodos = todos.filter((todo) => !!todo.completed).length;
   const totalTodos = todos.length;
 
+  const searchedTodos = todos.filter((todo) => {
+    const todoText = todo.text.toLowerCase();
+    const searchText = searchValue.toLowerCase();
+    return todoText.includes(searchText);
+  });
+
   console.log('Los usuarios buscan todos de ' + searchValue);
 
   return (
@@ -28,7 +34,7 @@ function App() {
       <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
 
       <TodoList>
-        {defaultTodos.map((todo) => (
+        {searchedTodos.map((todo) => (
           <TodoItem
             key={todo.text}
             text={todo.text}

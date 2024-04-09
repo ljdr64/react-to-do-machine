74073c6c51054fa149f2c82269b768c1d483b586
Laureano Rodriguez <laure.jd0101@gmail.com>
2024-04-09 19:27:59 -0300

    Iconos con colores dinamicos

--- src/DeleteIcon.js
+++ src/DeleteIcon.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { TodoIcon } from './TodoIcon';
 
-function DeleteIcon() {
-  return <TodoIcon type="delete" color="green" />;
+function DeleteIcon({ onDelete }) {
+  return <TodoIcon type="delete" color="gray" onClick={onDelete} />;
 }
 
 export { DeleteIcon };

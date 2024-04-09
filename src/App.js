bdff7e3eeb2cc65d74d7ab96ad81fbe273b20e50
Laureano Rodriguez <laure.jd0101@gmail.com>
2024-04-09 01:31:37 -0300

    Completando y eliminando TODOs

--- src/App.js
+++ src/App.js
@@ -26,7 +26,19 @@ function App() {
     return todoText.includes(searchText);
   });
 
-  console.log('Los usuarios buscan todos de ' + searchValue);
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text === text);
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text === text);
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  };
 
   return (
     <>
@@ -39,6 +51,8 @@ function App() {
             key={todo.text}
             text={todo.text}
             completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
           />
         ))}
       </TodoList>

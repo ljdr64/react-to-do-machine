bf8d2e503b4fe206a0b17b99b5bb78ec2b12a847
Laureano Rodriguez <laure.jd0101@gmail.com>
2024-04-17 23:41:07 -0300

    useContext

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,15 +1,20 @@
+import React from 'react';
+import { TodoContext } from '../TodoContext';
 import './TodoCounter.css';
 
-function TodoCounter({ total, completed }) {
-  return total === 0 ? (
+function TodoCounter() {
+  const { completedTodos, totalTodos } = React.useContext(TodoContext);
+
+  return totalTodos === 0 ? (
     <h1 className="TodoCounter">Aun no tienes TODOs</h1>
-  ) : total === completed ? (
+  ) : totalTodos === completedTodos ? (
     <h1 className="TodoCounter">
       ¡Felicidades por completar todos tus TODOs! 🎉👏
     </h1>
   ) : (
     <h1 className="TodoCounter">
-      Has completado <span>{completed}</span> de <span>{total}</span> TODOs
+      Has completado <span>{completedTodos}</span> de <span>{totalTodos}</span>{' '}
+      TODOs
     </h1>
   );
 }
